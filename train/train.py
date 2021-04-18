e1976c05735e47d2c30394a1fd11f3e97a2926ff
Michael Pieler <Michael.Pieler@Gmail.com>
2021-04-18 18:11:54 +0200

    Update step logging 3.

--- train/train.py
+++ train/train.py
@@ -232,7 +232,7 @@ def train_ddp(args, model, optimizer, dl_train, epochs, logger=None, writer=None
             if args.rank == 0:
                 writer.add_scalars("2 timings/1 step", {"dt": dt, "bt": bt}, step)
                 if (step % args.save_interval_step == 0) and (step != 0):
-                    logger.info(f"{datetime.now()} epoch: {epoch:>4} step: {step:<8} bt: {batch_time.avg:<10.3f}dt: {data_time.avg:<10.3f}{'train' if train else 'valid'} loss: {losses.avg:<10.3f}")
+                    logger.info(f"{datetime.now()} epoch: {epoch:>4} step: {step:>8} bt: {batch_time.avg:<10.3f}dt: {data_time.avg:<10.3f}{'train' if train else 'valid'} loss: {losses.avg:<10.3f}")
                 step += 1
 
             tp = time.time()
@@ -248,7 +248,7 @@ def train_ddp(args, model, optimizer, dl_train, epochs, logger=None, writer=None
         epoch_time = reduce_tensor(et, args.world_size)
 
         if args.rank == 0:
-            logger.info(f"{datetime.now()} epoch: {epoch:>4} et: {epoch_time:<10.3f}bt: {batch_time.avg:<10.3f}dt: {data_time.avg:<10.3f}{'train' if train else 'valid'} loss: {losses.avg:<10.3f}")
+            logger.info(f"{datetime.now()} epoch: {epoch:>4} et: {epoch_time:<11.3f}bt: {batch_time.avg:<10.3f}dt: {data_time.avg:<10.3f}{'train' if train else 'valid'} loss: {losses.avg:<10.3f}")
             writer.add_scalars("1 loss/2 epoch", {"train": losses.avg}, epoch)
             writer.add_scalars("2 timings/2 step", {"dt": data_time.avg, "bt": batch_time.avg}, epoch)
             writer.add_scalars("2 timings/3 epoch", {"et": epoch_time}, epoch)
